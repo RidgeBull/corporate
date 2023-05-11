df9b63a154ee1044be325fc2e5a53812d5cbf54c
knao124 <knao124@gmail.com>
2023-05-11 19:15:12 +0900

    update site info

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,11 +8,11 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `SAPY`,
+    siteTitleAlt: `SAPY - Lead the edge math optimization`,
+    siteHeadline: `SAPY - Lead the edge math optimization`,
+    siteUrl: `https://sapy.co.jp`,
+    siteDescription: `SAPY`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
     author: `@lekoarts_de`,
